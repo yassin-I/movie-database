9c73211a420d55d8940da107db6d7ed6de96f59d
MichaelBooi <michaelbooi@hotmail.com>
2016-10-08 11:24:37 +0200

    Validations for Movies

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,7 @@
 class Movie < ApplicationRecord
+  validates :name, presence: true
+  validates :name, uniqueness: true
+
+  validates :genre, length: { maximum: 25 }, allow_blank: true
+  validates :image_url, length: { maximum: 255 }, allow_blank: true
 end
